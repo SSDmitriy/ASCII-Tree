d5fd7e71461b15d24f578ebc00acad7bbc593afa
SSDmitiry <XYZmail@yandex.ru>
2022-11-05 00:28:15 +0500

    Add project files.

---
+++ ASCII-Tree/Vector2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Vector2
+{
+
+    public float x { get; private set; }
+    public float y { get; private set; }
+
+    public Vector2()
+    {
+        x = 0.0f;
+        y = 0.0f;
+    }
+
+    public Vector2(float x, float y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    //здесь перегружается оператор "+" https://metanit.com/sharp/tutorial/3.36.php
+    public static Vector2 operator +(Vector2 a, Vector2 b)
+    {
+        return new Vector2 { x = a.x + b.x, y = a.y + b.y };
+    }
+
+    public static Vector2 operator -(Vector2 a, Vector2 b)
+    {
+        return new Vector2 { x = a.x - b.x, y = a.y - b.y };
+    }
+
+
+
+}
